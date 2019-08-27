6f9b5369056e3475a2efffd44f67398f3e0dc75f
MiaoDragon <innocentmyl@gmail.com>
2019-08-26 18:29:44 -0700

    try 100x400 envs

--- scripts/experiments/simple/r3d/lightning_r3d.sh
+++ scripts/experiments/simple/r3d/lightning_r3d.sh
@@ -1,5 +1,5 @@
 cd ..
-python lightning_client.py --res_path ../../../results/simple/r3d/ --N 100 --NP 4000 --data_path ../../../data/simple/r3d/ \
+python lightning_client.py --res_path ../../../results/simple/r3d/ --N 100 --NP 400 --data_path ../../../data/simple/r3d/ \
 --env_type r3d
 # 100 4000
 cd r3d
