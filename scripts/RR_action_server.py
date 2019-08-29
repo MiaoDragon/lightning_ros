2b1b714e0645cf4338f3338bab37088a26e1ce85
MiaoDragon <innocentmyl@gmail.com>
2019-08-28 17:39:51 -0700

    try printing to see what is wrong

--- scripts/RR_action_server.py
+++ scripts/RR_action_server.py
@@ -474,6 +474,8 @@ class RRNode:
                             total_num_paths_NN += 1
                             total_new_node += len(repairedSection)
                             total_new_node_NN += len(repairedSection)
+                        rospy.loginfo('RR action server: total_new_node: %d' % (total_new_node))
+                        rospy.loginfo('RR action server: total_new_node_NN: %d' % (total_new_node_NN))
                         rospy.loginfo('RR action server: total_num_paths_NN: %d' % (total_num_paths_NN))
                         ## TODO: returning only invalid sections that are planned by classical method, and train only on them
                         ## TODO: modify library path format to add planner type, so we can train model according to it
