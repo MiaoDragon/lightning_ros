fab291c41a80daa3c39ec71bf5497509d21c1c6b
MiaoDragon <innocentmyl@gmail.com>
2019-09-14 21:43:26 -0700

    change number of data for s2d, now run s2d

--- scripts/experiments/simple/s2d/lightning_s2d.sh
+++ scripts/experiments/simple/s2d/lightning_s2d.sh
@@ -1,5 +1,5 @@
 cd ..
-python lightning_client.py --res_path ../../../results/simple/s2d/ --N 2 --NP 4 --data_path ../../../data/simple/s2d/ \
+python lightning_client.py --res_path ../../../results/simple/s2d/ --N 100 --NP 400 --data_path ../../../data/simple/s2d/ \
 --env_type s2d
 # 100 4000
 cd s2d
