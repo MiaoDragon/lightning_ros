5093108255fe1359c3f8ca7c7b53c2fd21e3e45b
MiaoDragon <innocentmyl@gmail.com>
2019-10-14 15:18:32 -0700

    change device to GPU, and set number of paths to 1000 in Baxter

--- scripts/experiments/baxter/lightning_baxter.sh
+++ scripts/experiments/baxter/lightning_baxter.sh
@@ -2,4 +2,4 @@ python lightning_client.py \
 --env_data_path ../../../data/baxter/env/environment_data/ --path_data_path ../../../data/baxter/test/paths/ --pointcloud_data_path ../../../data/baxter/test/pcd/ \
 --envs_file trainEnvironments.pkl --path_data_file trainEnvironments_testPaths.pkl \
 --good_path_sample_path ../../../results/baxter/path_samples/good_path_samples --bad_path_sample_path ../../../results/baxter/path_samples/bad_path_samples \
---NP 100 --N 10
+--NP 1000 --N 10
