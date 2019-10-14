d4844ac00252e1797e34e7bf1ca8f15653325c2c
MiaoDragon <innocentmyl@gmail.com>
2019-10-14 15:28:06 -0700

    now try using train paths instead of test paths

--- scripts/experiments/baxter/lightning_baxter.sh
+++ scripts/experiments/baxter/lightning_baxter.sh
@@ -1,5 +1,5 @@
 python lightning_client.py \
---env_data_path ../../../data/baxter/env/environment_data/ --path_data_path ../../../data/baxter/test/paths/ --pointcloud_data_path ../../../data/baxter/test/pcd/ \
---envs_file trainEnvironments.pkl --path_data_file trainEnvironments_testPaths.pkl \
+--env_data_path ../../../data/baxter/env/environment_data/ --path_data_path ../../../data/baxter/train/paths/ --pointcloud_data_path ../../../data/baxter/test/pcd/ \
+--envs_file trainEnvironments.pkl --path_data_file trainPaths.pkl \
 --good_path_sample_path ../../../results/baxter/path_samples/good_path_samples --bad_path_sample_path ../../../results/baxter/path_samples/bad_path_samples \
 --NP 1000 --N 10
