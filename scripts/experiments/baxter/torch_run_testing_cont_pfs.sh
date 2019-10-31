b398a3f0eff3e0d588d66f5c27eff1b7ec225cac
MiaoDragon <innocentmyl@gmail.com>
2019-10-30 17:02:21 -0700

    added testing for baxter

---
+++ scripts/experiments/baxter/torch_run_testing_cont_pfs.sh
@@ -0,0 +1,5 @@
+python torch_test_cont.py \
+--env_data_path ./env/environment_data/ --path_data_path ../../../data/baxter/test/paths/ --pointcloud_data_path ../../../data/baxter/test/pcd/ \
+--envs_file trainEnvironments.pkl --path_data_file trainEnvironments_testPaths.pkl \
+--model_path ../../../results/baxter/ --model_name model_baxter.pkl --experiment_name lightning_pfs --device 2 --N 10 --NP 100 \
+--good_path_sample_path ../../../results/baxter/test/pfs/path_samples/good_path_samples --bad_path_sample_path ../../../results/baxter/test/pfs/path_samples/bad_path_samples
